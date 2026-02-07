4e7a627364da365950a1eee33b2d130f7950747c
Letheriver2007 <25007933@siswa.um.edu.my>
2026-02-07 12:33:56 +0800

    Feat SugarSql column

--- CyreneMvvm.SourceGenerator/GeneratorHelper.cs
+++ CyreneMvvm.SourceGenerator/GeneratorHelper.cs
@@ -11,6 +11,26 @@ public static class GeneratorHelper
     public const string ObservableList = "CyreneMvvm.Model.ObservableList";
     public const string ObservableDictionary = "CyreneMvvm.Model.ObservableDictionary";
     public const string ObProp = "CyreneMvvm.Attributes.ObPropAttribute";
+    public const string ObColumn = "CyreneMvvm.Attributes.ObColumnAttribute";
+
+    public static bool IsPrimary(ITypeSymbol typeSymbol)
+    {
+        var actualType = typeSymbol;
+        if (typeSymbol is INamedTypeSymbol { OriginalDefinition.SpecialType: SpecialType.System_Nullable_T } namedType)
+            actualType = namedType.TypeArguments[0];
+
+        if (actualType.TypeKind == TypeKind.Enum) return true;
+        return actualType.SpecialType switch
+        {
+            SpecialType.System_Boolean or SpecialType.System_Char or
+            SpecialType.System_SByte or SpecialType.System_Byte or
+            SpecialType.System_Int16 or SpecialType.System_UInt16 or
+            SpecialType.System_Int32 or SpecialType.System_UInt32 or
+            SpecialType.System_Int64 or SpecialType.System_UInt64 or
+            SpecialType.System_Single or SpecialType.System_Double or SpecialType.System_Decimal => true,
+            _ => false
+        };
+    }
 
     public static bool IsObservableObject(INamedTypeSymbol classSymbol)
     {
@@ -48,9 +68,29 @@ public static class GeneratorHelper
         return false;
     }
 
+    public static bool HasObservableColumnAttr(PropertyDeclarationSyntax prop, SemanticModel model)
+    {
+        foreach (var attributeList in prop.AttributeLists)
+            foreach (var attribute in attributeList.Attributes)
+            {
+                var symbol = model.GetSymbolInfo(attribute).Symbol?.ContainingType;
+                if (symbol != null && symbol.ToDisplayString().Contains(ObColumn)) return true;
+            }
+        return false;
+    }
+
+    public static bool HasObservableColumnAttr(IPropertySymbol propSymbol)
+    {
+        foreach (var attribute in propSymbol.GetAttributes())
+            if (attribute.AttributeClass?.ToDisplayString().Contains(ObColumn) == true)
+                return true;
+        return false;
+    }
+
     public static bool ShouldGenProp(PropertyDeclarationSyntax prop, SemanticModel model)
     {
-        return IsPartialProperty(prop) && HasObservablePropAttr(prop, model) && !IsObservableCollection(prop, model);
+        return IsPartialProperty(prop) && !IsObservableCollection(prop, model) &&
+            (HasObservablePropAttr(prop, model) || HasObservableColumnAttr(prop, model));
     }
 
     public static bool ShouldGenCollection(PropertyDeclarationSyntax prop, SemanticModel model)
