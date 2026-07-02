a6dae3aa1e0feb61bf6e9802cf6bb32ef4ac726e
Letheriver2007 <25007933@siswa.um.edu.my>
2026-07-02 15:11:04 +0800

    Fix string column bug

--- CyreneMvvm.SourceGenerator/GeneratorHelper.cs
+++ CyreneMvvm.SourceGenerator/GeneratorHelper.cs
@@ -23,11 +23,12 @@ public static class GeneratorHelper
         if (actualType.TypeKind == TypeKind.Enum) return true;
         return actualType.SpecialType switch
         {
-            SpecialType.System_Boolean or SpecialType.System_Char or
+            SpecialType.System_Boolean or
             SpecialType.System_SByte or SpecialType.System_Byte or
             SpecialType.System_Int16 or SpecialType.System_UInt16 or
             SpecialType.System_Int32 or SpecialType.System_UInt32 or
             SpecialType.System_Int64 or SpecialType.System_UInt64 or
+            SpecialType.System_Char or SpecialType.System_String or
             SpecialType.System_Single or SpecialType.System_Double or SpecialType.System_Decimal => true,
             _ => false
         };
